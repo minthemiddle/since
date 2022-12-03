a62d730cb5d74fae049f348829a04940ebe89857
Martin Betz <martinmichaelbetz@gmail.com>
2022-12-03 20:48:00 +0100

    Fix: Mobile view

--- resources/views/entries/index.blade.php
+++ resources/views/entries/index.blade.php
@@ -1,22 +1,22 @@
 <x-app-layout>
     <table class="">
         <thead>
-            <tr >
-                <th class="font-normal italic">Title</th>
-                <th class="font-normal italic">Last Update</th>
-                <th class="font-normal italic">#</th>
-                <th class="font-normal italic"></th>
+            <tr>
+                <th class="text-left font-normal italic">Title</th>
+                <th class="text-left font-normal italic">Last Update</th>
+                <th class="text-center font-normal italic">#</th>
+                <th></th>
             </tr>
         </thead>
         <tbody>
             
         @foreach ($entries as $entry)
         <tr>
-                <td class="px-3 py-2"><a href="{{ route('entries.show', $entry) }}"><strong>{{ $entry->title }}</strong></a></td>
-                <td class="px-3 py-2"><span title="{{ $entry->updated_at->format('d.m.y H:i') }}">
-                {{ $entry->updated_at->diffForHumans() }}</span></td>
-                <td class="px-3 py-2">{{ $entry->count }}x</td>
-                <td class="px-3 py-2">
+                <td class="px-1 md:px-3 py-1 md:py-2"><a href="{{ route('entries.show', $entry) }}"><strong>{{ $entry->title }}</strong></a></td>
+                <td class="px-1 md:px-3 py-1 md:py-2"><span title="{{ $entry->updated_at->format('d.m.y H:i') }}">
+                {{ $entry->updated_at->shortRelativeDiffForHumans() }}</span></td>
+                <td class="px-1 md:px-3 py-1 md:py-2">{{ $entry->count }}x</td>
+                <td class="px-1 md:px-3 py-1 md:py-2">
                     @if ($entry->user->is(auth()->user()))
                     <form method="POST" class="inline" action="{{ route('entries.update', $entry) }}">
                         @csrf @method('patch')
