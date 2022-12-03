a3239114978981293058caade875cf36945e9a1e
Martin Betz <martinmichaelbetz@gmail.com>
2022-12-03 13:14:15 +0100

    Reformat: Use table instead of unordered list

--- resources/views/entries/index.blade.php
+++ resources/views/entries/index.blade.php
@@ -1,32 +1,52 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Entries') }}
-        </h2>
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">{{ __('Entries') }}</h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <ul class="list-disc">
+                    <table >
+                        <thead>
+                            <tr>
+                                <th class="bg-gray-100">Title</th>
+                                <th class="bg-gray-100">Last Update</th>
+                                <th class="bg-gray-100">Count</th>
+                                <th class="bg-gray-100">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
                         @foreach($entries as $entry)
-                            <li><strong>{{ $entry->title }}</strong>, <span title="{{ $entry->updated_at->format('d.m.y H:i') }}"> {{ $entry->updated_at->diffForHumans() }}</span> ({{ $entry->count }}x), @if ($entry->user->is(auth()->user()))<form method="POST" class="inline" action="{{ route('entries.update', $entry) }}"> @csrf @method('patch')<input type="submit" value="Update" class="underline"><input type="hidden" name="title" value="{{ $entry->title }}"></form> @endif</li>
+                        <tr>
+                                <td class="px-3 py-2 bg-gray-50"><strong>{{ $entry->title }}</strong></td>
+                                <td class="px-3 py-2"><span title="{{ $entry->updated_at->format('d.m.y H:i') }}">
+                                {{ $entry->updated_at->diffForHumans() }}</span></td>
+                                <td class="px-3 py-2 bg-gray-50">{{ $entry->count }}x</td>
+                                <td class="px-3 py-2">
+                                    @if ($entry->user->is(auth()->user()))
+                                    <form method="POST" class="inline" action="{{ route('entries.update', $entry) }}">
+                                        @csrf @method('patch')
+                                        <input type="submit" value="Update" class="underline" />
+                                        <input type="hidden" name="title" value="{{ $entry->title }}"/>
+                                    </form>
+                                    @endif
+                                </td>
+                        </tr>
                         @endforeach
-                    </ul>
+                        </tbody>
+                    </table>
                 </div>
-        <div class="p-6">
-        <form method="POST" action="{{ route('entries.store') }}">
-            @csrf
-            <input type="text" name="title" value="{{ old('title') }}">
-            <x-input-error :messages="$errors->get('message')" class="mt-2" />
-            <x-primary-button class="mt-4">{{ __('Save') }}</x-primary-button>
-        </form>
-    </div>
+                <div class="p-6">
+                    <form method="POST" action="{{ route('entries.store') }}">
+                        @csrf
+                        <input type="text" name="title" value="{{ old('title') }}" />
+                        <x-input-error :messages="$errors->get('message')" class="mt-2" />
+                        <x-primary-button class="mt-4">{{ __('Save') }}</x-primary-button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-    
-    
-    
 </x-app-layout>
