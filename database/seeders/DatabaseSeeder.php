f4bc312c2fb970f8cd27212c67d2fa67ea00080c
Martin Betz <martinmichaelbetz@gmail.com>
2022-12-03 20:27:48 +0100

    Add: User and Entry seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Entry;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +18,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Test',
+            'email' => 'test@test.test',
+        ]);
+        
+        Entry::factory()->create([
+            'title' => '🧠 Did brain jogging',
+            'comment' => 'Doing a puzzle for it',
+            'due' => 3*30*24*60, // 3 months, 30 days, 24 hours, 60 minutes
+        ]);
+        
+        Entry::factory()->create([
+            'title' => '🧃 Drank juice',
+            'comment' => 'Quite a bit actually…',
+            'due' => 21*24*60, // 21 days, 24 hours, 60 minutes
+        ]);
     }
 }
