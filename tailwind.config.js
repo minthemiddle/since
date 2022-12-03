2292a59e569ce9aac7dc333df385b38276820173
Martin Betz <martinmichaelbetz@gmail.com>
2022-12-03 13:13:49 +0100

    Cut: Nunito font

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,7 @@ module.exports = {
     ],
 
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
-        },
+
     },
 
     plugins: [require('@tailwindcss/forms')],
