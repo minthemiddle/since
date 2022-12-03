2368b45b62bf2cd681b47c4f4c54744e1aa60371
Martin Betz <martinmichaelbetz@gmail.com>
2022-12-03 15:03:12 +0100

    Reformat: Navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('entries.index')" :active="request()->routeIs('entries.index')">
+                        {{ __('Entries') }}
+                    </x-nav-link>
                 </div>
             </div>
 
