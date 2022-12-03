950301cd5e12d6026bcda386fef78172142907d8
Martin Betz <martinmichaelbetz@gmail.com>
2022-12-03 20:27:41 +0100

    Add: EntryFactory

---
+++ database/factories/EntryFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Entry>
+ */
+class EntryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->word(),
+            'comment' => fake()->sentences(1, true),
+            'due' => fake()->randomNumber(3),
+            'user_id' => User::find(1),
+        ];
+    }
+}
